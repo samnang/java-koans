56cc5b203f943cccd3e8a07d0be30d1bb4f3b1f9
Samnang Chhun <samnang.chhun@gmail.com>
2021-01-26 21:24:02 +0700

    Solve koans

--- koans/src/intermediate/AboutEquality.java
+++ koans/src/intermediate/AboutEquality.java
@@ -12,20 +12,20 @@ public class AboutEquality {
     public void sameObject() {
         Object a = new Object();
         Object b = a;
-        assertEquals(a == b, __);
+        assertEquals(a == b, true);
     }
 
     @Koan
     public void equalObject() {
         Integer a = new Integer(1);
         Integer b = new Integer(1);
-        assertEquals(a.equals(b), __);
-        assertEquals(b.equals(a), __);
+        assertEquals(a.equals(b), true);
+        assertEquals(b.equals(a), true);
     }
 
     @Koan
     public void noObjectShouldBeEqualToNull() {
-        assertEquals(new Object().equals(null), __);
+        assertEquals(new Object().equals(null), false);
     }
 
     static class Car {
@@ -42,13 +42,18 @@ public class AboutEquality {
             // Change this implementation to match the equals contract
             // Car objects with same horsepower and name values should be considered equal
             // http://download.oracle.com/javase/6/docs/api/java/lang/Object.html#equals(java.lang.Object)
+            if (other instanceof Car) {
+                Car otherCar = (Car)other;
+                return name.equals(otherCar.name) && horsepower == otherCar.horsepower;
+            }
+
             return false;
         }
 
         @Override
         public int hashCode() {
             // @see http://download.oracle.com/javase/6/docs/api/java/lang/Object.html#hashCode()
-            return super.hashCode();
+            return name.hashCode() * Integer.valueOf(horsepower).hashCode();
         }
     }
 
@@ -119,8 +124,8 @@ public class AboutEquality {
         Chicken chicken1 = new Chicken();
         chicken1.color = "black";
         Chicken chicken2 = new Chicken();
-        assertEquals(chicken1.equals(chicken2), __);
-        assertEquals(chicken1.hashCode() == chicken2.hashCode(), __);
+        assertEquals(chicken1.equals(chicken2), false);
+        assertEquals(chicken1.hashCode() == chicken2.hashCode(), true);
         // Does this still fit the hashCode contract? Why (not)?
         // Fix the Chicken class to correct this.
     }
--- koans/src/intermediate/AboutFileIO.java
+++ koans/src/intermediate/AboutFileIO.java
@@ -13,16 +13,16 @@ public class AboutFileIO {
     @Koan
     public void fileObjectDoesntCreateFile() {
         File f = new File("i-never.exist");
-        assertEquals(f.exists(), __);
+        assertEquals(f.exists(), false);
     }
 
     @Koan
     public void fileCreationAndDeletion() throws IOException {
         File f = new File("foo.txt");
         f.createNewFile();
-        assertEquals(f.exists(), __);
+        assertEquals(f.exists(), true);
         f.delete();
-        assertEquals(f.exists(), __);
+        assertEquals(f.exists(), false);
     }
 
     @Koan
@@ -40,10 +40,10 @@ public class AboutFileIO {
         size = fr.read(in);
         // No flush necessary!
         fr.close();
-        assertEquals(size, __);
+        assertEquals(size, data.length());
         String expected = new String(in);
-        assertEquals(expected.length(), __);
-        assertEquals(expected, __);
+        assertEquals(expected.length(), in.length);
+        assertEquals(expected, data);
         file.delete();
     }
 
@@ -61,9 +61,9 @@ public class AboutFileIO {
         BufferedReader br = null;
         try {
             br = new BufferedReader(fr);
-            assertEquals(br.readLine(), __); // first line
-            assertEquals(br.readLine(), __); // second line
-            assertEquals(br.readLine(), __); // what now?
+            assertEquals(br.readLine(), "First line"); // first line
+            assertEquals(br.readLine(), "Second line"); // second line
+            assertEquals(br.readLine(), null); // what now?
         } finally {
             // anytime you open access to a file, you should close it or you may
             // lock it from other processes (ie frustrate people)
@@ -90,6 +90,25 @@ public class AboutFileIO {
         pw.close();
 
         StringBuffer sb = new StringBuffer();
+        FileReader fr = new FileReader(file);
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(fr);
+            String line = br.readLine();
+            while(line != null) {
+                sb.append(line);
+
+                line = br.readLine();
+                if (line != null) {
+                    sb.append("\n");
+                }
+            }
+        } finally {
+            // anytime you open access to a file, you should close it or you may
+            // lock it from other processes (ie frustrate people)
+            closeStream(br);
+        }
+
         // Add the loop to go through the file line by line and add the line
         // to the StringBuffer
         assertEquals(sb.toString(), "1. line\n2. line");
